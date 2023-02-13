f2ffd3b5689c33d51504210c86ad93e239913864
victoria-parker <victoriaparkerb@gmail.com>
2023-02-13 16:18:39 +0100

    home controller and route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,5 +25,5 @@ Route::get('/register', [RegisterController::class,'show']);
 Route::post('/register',[RegisterController::class,'register']);
 Route::get('/login', [LoginController::class,'show']);
 Route::post('/login',[LoginController::class,'login']);
-
+Route::get('/home', [HomeController::class,'show']);
 
