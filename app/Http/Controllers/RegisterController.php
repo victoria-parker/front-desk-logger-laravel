cc5d40123e6b1a372c1f9cec5f05eef4ac12fdf8
victoria-parker <victoriaparkerb@gmail.com>
2023-02-13 13:32:25 +0100

    register function done

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function register($request){
-
+    public function register(RegisterRequest $request){
+        $user=User::create($request->validated());
+        return redirect('/login')->with('success','Account created successfully');
     }
 }
