672452a2c457a8c5f992ace7b61d7273f925dd6e
victoria-parker <victoriaparkerb@gmail.com>
2023-02-15 10:22:38 +0100

    added bootstrap to register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
-</head>
-<body>
+@extends('layouts.auth-master')
+
+@section('content')
+
+<h1>Register your company</h1>
+
     <form action="/register" method="post">
         @csrf
-        <input type="text" name="company" id="company" placeholder="company">
-        <input type="text" name="name" id="name" placeholder="name">
-        <input type="text" name="lastname" id="lastname" placeholder="lastname">
-        <input type="email" name="email" id="email" placeholder="email">
-        <input type="password" name="password" id="password" placeholder="password">
-        <input type="password" name="password_confirmation" id="password_confirmation" placeholder="password_confirmation">
 
-        <button type="submit">Register</button>
+        <div class="mb-3 form-floating">
+            <input type="text" placeholder="Company" class="form-control" id="company"  name="company" >
+          <label for="company" class="form-label">Company</label>
+        </div>
+
+        <div class="mb-3 form-floating">
+            <input type="text" placeholder="Name" class="form-control" id="name"  name="name" >
+          <label for="name" class="form-label">Name</label>
+        </div>
+
+        <div class="mb-3 form-floating">
+            <input type="text" placeholder="Last Name" class="form-control" id="lastname"  name="lastname" >
+          <label for="lastname" class="form-label">Last Name</label>
+        </div>
+
+        <div class="mb-3 form-floating">
+            <input type="email" placeholder="Email address" class="form-control" id="email" name="email">
+            <label for="email" class="form-label">Email address</label>
+          </div>
+
+          <div class="mb-3 form-floating">
+            <input type="password" placeholder="Password" class="form-control" id="password" name="password">
+            <label for="password" class="form-label">Password</label>
+        </div>
+
+        <div class="mb-3 form-floating">
+            <input type="password" placeholder="Confirm Password" class="form-control" id="password_confirmation" name="password_confirmation">
+            <label for="password_confirmation" class="form-label">Confirm your Password</label>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Register</button>
 
     </form>
-</body>
-</html>
+@endsection
