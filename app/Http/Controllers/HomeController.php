4cfba44b0b697944af3538d402212334941d961b
victoria-parker <victoriaparkerb@gmail.com>
2023-02-17 17:42:03 +0100

    filter by company added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,19 +8,13 @@ use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    public function show(){
-        if(!Auth::check()){
-            return redirect('/login');
-        }
-        return view('home.index');
-    }
 
     public function index(){
         if(!Auth::check()){
             return redirect('/login');
         }
         
-        $issues=Issue::all();
+        $issues=Issue::all()->where('company',Auth::user()->company);
 
         return view('home.index', ['issues'=>$issues]);
     }
