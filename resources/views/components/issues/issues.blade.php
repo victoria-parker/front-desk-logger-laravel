52686d7df494369a019bc4dee02af8e150a8792c
victoria-parker <victoriaparkerb@gmail.com>
2023-02-17 16:25:07 +0100

    modals with adding forms added

--- resources/views/components/issues/issues.blade.php
+++ resources/views/components/issues/issues.blade.php
@@ -39,3 +39,38 @@
 
 </tbody>
 </table>
+
+<!-- modal add issue -->
+<div class="modal fade" id="addIssue" tabindex="-1" aria-labelledby="issueModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="issueModalLabel">Add an Issue</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form method="post" action="issues/createIssue">
+                <div class="mb-3">
+                  <label for="roomNumber" class="col-form-label">Room number:</label>
+                  <input type="number" class="form-control" id="roomNumber" name="roomNumber">
+                </div>
+
+                <div class="mb-3">
+                    <label for="guestName" class="col-form-label">Guest Name:</label>
+                    <input type="text" class="form-control" id="guestName" name="guestName">
+                </div>
+
+                <div class="mb-3">
+                  <label for="text" class="col-form-label">Issue:</label>
+                  <textarea class="form-control" id="text" name="text"></textarea>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+              </form>
+        </div>
+      </div>
+    </div>
+</div>
--- resources/views/components/wakeupcalls/wakeupcalls.blade.php
+++ resources/views/components/wakeupcalls/wakeupcalls.blade.php
@@ -40,3 +40,43 @@
 
     </tbody>
 </table>
+
+ <!-- modal add wakeUpCall -->
+ <div class="modal fade" id="addWakeUpCall" tabindex="-1" aria-labelledby="wakeUpCallModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="wakeUpCallModalLabel">Add WakeUpCall</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <form method="post" action="wakeUpCalls/createWakeUpCall">
+                <div class="mb-3">
+                  <label for="roomNumber" class="col-form-label">Room number:</label>
+                  <input type="number" class="form-control" id="roomNumber" name="roomNumber">
+                </div>
+
+                <div class="mb-3">
+                    <label for="wakeUpTime" class="col-form-label">Wake up time time:</label>
+                    <input type="datetime-local" class="form-control" id="wakeUpTime" name="wakeUpTime">
+                </div>
+
+                <div class="mb-3">
+                    <label for="guestName" class="col-form-label">Guest Name:</label>
+                    <input type="text" class="form-control" id="guestName" name="guestName">
+                  </div>
+
+                <div class="mb-3">
+                  <label for="observations" class="col-form-label">observations:</label>
+                  <textarea class="form-control" id="observations" name="observations"></textarea>
+                </div>
+
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+              </form>
+        </div>
+      </div>
+    </div>
+</div>
