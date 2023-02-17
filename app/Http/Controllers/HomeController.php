efcec8cdb44d7c0db1a07c973aa394a6a24bd89b
victoria-parker <victoriaparkerb@gmail.com>
2023-02-17 14:26:59 +0100

    fontawesome added and included in tables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,4 +13,14 @@ class HomeController extends Controller
         }
         return view('home.index');
     }
+
+    public function index(){
+        if(!Auth::check()){
+            return redirect('/login');
+        }
+
+    }
+
+
+
 }
