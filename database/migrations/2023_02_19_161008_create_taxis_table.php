e97bf8313fc1008a32cd7830a29034ba697d7b2d
victoria-parker <victoriaparkerb@gmail.com>
2023-02-19 17:12:46 +0100

    taxis model made

---
+++ database/migrations/2023_02_19_161008_create_taxis_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxis', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('created_by');
+            $table->integer('room_number');
+            $table->dateTime('pick_up_time');
+            $table->boolean('resolved');
+            $table->string('observations')->nullable();
+            $table->string('company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxis');
+    }
+};
